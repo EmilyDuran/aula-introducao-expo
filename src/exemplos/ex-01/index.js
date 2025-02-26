26472534140742154200b9be19f131232c06dca6
EmilyDuran <emilydiane27@outlook.com>
2025-02-26 20:41:13 -0300

    Aula Introdução

---
+++ src/exemplos/ex-01/index.js
@@ -0,0 +1,13 @@
+import { View,Text } from 'react-native';
+
+import styles from './styles';
+
+function Exemplo1 () {
+    return(
+        <View styles={styles.container}>
+            <Text>Exemplo 1</Text>
+        </View>
+    )
+}
+
+export default Exemplo1;
