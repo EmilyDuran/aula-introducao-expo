8e6b2549de137c9a71540661568b58ef7e9c919a
EmilyDuran <emilydiane27@outlook.com>
2025-03-18 20:39:25 -0300

    props

--- src/exemplos/mensagem/index.js
+++ src/exemplos/mensagem/index.js
@@ -6,6 +6,11 @@ function Mensagem (props) {
     return(
         <View styles={styles.container}>
             <Text style={styles.titulo}>{props.titulo}</Text>
+            <Text style={styles.texto}>{props.mensagem}</Text>
+
+            <Mensagem titulo={'ERRO'} mensagem={'Você não clicou corretamente!'}></Mensagem>
+            <Mensagem titulo={'ERRO'} mensagem={'Você não clicou corretamente!'}></Mensagem>
+            <Mensagem titulo={'ERRO'} mensagem={'Você não clicou corretamente!'}></Mensagem>
         </View>
     )
 }
