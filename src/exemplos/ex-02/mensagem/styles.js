2b026a966bd4ab4396f1e23131b679437088e2c5
EmilyDuran <emilydiane27@outlook.com>
2025-03-19 19:22:34 -0300

    substituição

--- src/exemplos/ex-02/mensagem/styles.js
+++ src/exemplos/ex-02/mensagem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from 'react-native';
+import {  StyleSheet } from 'react-native';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#deepskyblue',
+        backgroundColor: 'deepskyblue',
         borderWidth: RFPercentage(0.6),
         width: '90%',
         padding: 8,
@@ -11,19 +11,17 @@ const styles = StyleSheet.create({
         borderRadius: 20,
     },
     titulo: {
-        fontWeight:'bold',
-        color: '#121212',
         fontSize: RFPercentage(3),
+        color: '#121212',   
+        fontWeight: 'bold',
         marginBottom: RFPercentage(1),
+       
     },
     texto: {
         fontSize: RFPercentage(2.2),
         color: '#fafafa',
     },
-    imagem: {
-        height: RFPercentage(10),
-        width: RFPercentage(28),
-    },
+    
 });
 
 export default styles;
