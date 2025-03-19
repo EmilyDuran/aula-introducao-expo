9ce1b52f66f5877e0ced04f35bdb0d0dd9b5538f
EmilyDuran <emilydiane27@outlook.com>
2025-03-19 19:27:27 -0300

    substituição

--- src/exemplos/ex-01/index.js
+++ src/exemplos/ex-01/index.js
@@ -1,13 +1,14 @@
-import { View,Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import styles from './styles';
 
 function Exemplo1 () {
-    return(
-        <View styles={styles.container}>
-            <Text>Exemplo 1</Text>
-        </View>
-    )
+  return(
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Exemplo 1</Text>
+      <Text style={styles.texto}>Aula de react-native com Expo</Text>
+    </View>
+  )
 }
 
 export default Exemplo1;
