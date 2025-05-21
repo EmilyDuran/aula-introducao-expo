bac4af8c5fd8b66d660f31cdb74ab6aa35512a5f
EmilyDuran <emilydiane27@outlook.com>
2025-05-21 20:20:12 -0300

    oii

--- App.js
+++ App.js
@@ -13,12 +13,13 @@ import Exemplo4 from './src/exemplos/ex-04';
 import Exemplo5 from './src/exemplos/ex-05';
 import Exemplo6 from './src/exemplos/ex-06';
 import Atividade6 from './src/atividade/atividade-06';
+import Exemplo8 from './src/exemplos/ex-08';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade6/>
+      <Exemplo8/>
     </View>
   );
 }
